b211bee57b901a67e79ac2ec253d707fcdbb2f74
thraraujo <thgr.araujo@gmail.com>
2024-05-29 10:14:29 -0300

    fix some mistakes

--- web-scraping/pci-jobs.sh
+++ web-scraping/pci-jobs.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+BASE_DIR="/home/thiago/.config/scripts/web-scraping/" 
 FILENAME=$(date +%F.org)
 
-touch "./jobs/$FILENAME" 
+touch "$BASE_DIR/jobs/$FILENAME" 
+
+OUTPUT_FILE="$BASE_DIR/jobs/$FILENAME"
 
 echo "File $FILENAME created at $(date)"
 
 echo "Searching for jobs..."
 
-echo "*** $(date)" >> ./jobs/$FILENAME
-echo " " >> ./jobs/$FILENAME
+echo "*** $(date)" >> "$OUTPUT_FILE"
+echo " " >> "$OUTPUT_FILE"
 
-python3 pci-ultimas.py >> ./jobs/$FILENAME
-echo " " >> ./jobs/$FILENAME
+python3 $BASE_DIR/pci-ultimas.py >> "$OUTPUT_FILE"
+
+echo " " >> "$OUTPUT_FILE"
 
 echo "added some jobs to $FILENAME."
