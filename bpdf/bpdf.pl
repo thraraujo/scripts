1450d493db76bac70e7a6549e0136abb4ef5fdb7
thraraujo <thgr.araujo@gmail.com>
2024-05-31 15:28:03 -0300

    fix typos

--- bpdf/bpdf.pl
+++ bpdf/bpdf.pl
@@ -13,14 +13,14 @@ open(my $fh, '<', $filename) or die "Could not open file '$filename' for reading
 my $first_line = <$fh>;
 
 if ($first_line =~ /pdflatex/) {
-    print "creating pdf with pdflatex...\n";
+    print "Creating pdf with pdflatex...\n";
     my $my_command = qx(pdflatex $filename);
     print $my_command;
 } elsif ($first_line =~ /lualatex/) {
-    print "create a pdf with lualatex...\n";
+    print "Create a pdf with lualatex...\n";
     my $my_command = qx(lualatex $filename);
     print $my_command;
 } else {
-    print "unknown engine!"
+    print "Unknown engine!"
 }
   
