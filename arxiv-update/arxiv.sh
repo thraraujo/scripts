64e81d64fcbe1ec7f62c6623f0213a272abd7454
thraraujo <thgr.araujo@gmail.com>
2024-06-07 09:15:55 -0300

    finish arxiv script

--- arxiv-update/arxiv.sh
+++ arxiv-update/arxiv.sh
@@ -15,9 +15,18 @@ do
 
     python3 arxiv.py "$url" > $TEMP_FILE
 
-    grep -i -e 'vieira' -e 'bogomolny' -e 'carrollian' $TEMP_FILE >> $OUTPUT_FILE
+    LOOP_AUTHORS=$(wc -l authors.org | awk '{print $1}')
+    LOOP_FIELDS=$(wc -l fields.org | awk '{print $1}')
+
+    for ((n=1; n <= LOOP_AUTHORS ; n++)) ; do
+        author=$(awk "NR==$n {print \$1}" authors.org)
+        grep -i "$author" $TEMP_FILE >> $OUTPUT_FILE
+    done
+
+    for ((n=1; n <= LOOP_FIELDS ; n++)) ; do
+        field=$(awk "NR==$n {print \$1}" fields.org)
+        grep -i "$field" $TEMP_FILE >> $OUTPUT_FILE
+    done
 
     rm "$TEMP_FILE"
 done
-
-#python3 arxiv.py  
