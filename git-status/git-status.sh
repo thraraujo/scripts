3be3f22dd56db09309ff90e77ee287cfaf6635ed
thraraujo <thgr.araujo@gmail.com>
2024-08-23 09:05:25 -0300

    organize git-status script

--- git-status/git-status.sh
+++ git-status/git-status.sh
@@ -9,47 +9,45 @@ gitRepos=(
     $HOME/Sync/projects/physics-math/
     $HOME/Sync/projects/wiki/
     $HOME/Sync/projects/work/research-reviews/
-    $HOME/Sync/projects/work/q-bosons/
-    $HOME/Sync/projects/work/pySymmPol
+    $HOME/Sync/projects/work/pySymmPol/
+    $HOME/Sync/projects/work/algebras-lie/
 )
 
-
-echo "Here are the options you have:"
-echo "    1. See status."
-echo "    2. See Modified files."
-
-read -p ">> What option do you want? " CHOICE
-echo " "
-
+# echo "Here are the options you have:"
+# echo "    1. See status."
+# echo "    2. See Modified files."
+# 
+# read -p ">> What option do you want? " CHOICE
+# echo " "
 
 
-if [ $CHOICE -eq 1 ] ; then
-echo "Option 1: See modified repos."
-echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
+
+# if [ $CHOICE -eq 1 ] ; then
+# echo "Option 1: See modified repos."
+
+echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
 for repo in "${gitRepos[@]}"
 do
     test -n "$(git -C $repo status --porcelain)" && echo "   >> MODIFIED :: $repo" #|| echo "<< NOTHING :: $repo"
 done
-echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
-fi
+echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
+# fi
 
-
-
-if [ $CHOICE -eq 2 ] ; then
-echo "Option 2: See modified files in the repos."
-
-for ((i=0; i<${#gitRepos[@]}; i++))
-do
-   echo "    $i - ${gitRepos[i]}"
-done
-
-echo " "
-
-read -p " What directory do you want? " DIR
-
-cd "${gitRepos[$DIR]}"
-
-git status
-
-fi
+# if [ $CHOICE -eq 2 ] ; then
+# echo "Option 2: See modified files in the repos."
+# 
+# for ((i=0; i<${#gitRepos[@]}; i++))
+# do
+#    echo "    $i - ${gitRepos[i]}"
+# done
+# 
+# echo " "
+# 
+# read -p " What directory do you want? " DIR
+# 
+# cd "${gitRepos[$DIR]}"
+# 
+# git status
+# 
+# fi
 
